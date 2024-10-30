f417d30d5a902b99be526e80b183b2f44839fc7b
campanula <65985506+campanula@users.noreply.github.com>
2024-10-30 16:52:59 +0000

    test2

--- src/test/java/tdd/HighestNumberFinderTest.java
+++ src/test/java/tdd/HighestNumberFinderTest.java
@@ -8,7 +8,7 @@ public class HighestNumberFinderTest {
 
     // singleItem array should return the single item
     @Test
-    public void find_highest_in_array_of_one_expect_single_item(){
+    public void find_highest_in_array_of_one_expect_single_item() {
         // arrange
         HighestNumberFinder cut = new HighestNumberFinder();
         int[] input = {10};
@@ -21,7 +21,18 @@ public class HighestNumberFinderTest {
         assertEquals(expectedResult, actualResult);
     }
 
-    // findHighestNumber will return number
+    // findHighestNumber will return 13 from 7, 13
+    public void find_highest_in_array_of_two_descending_expect_first_element() {
+        // arrange
+        HighestNumberFinder cut = new HighestNumberFinder();
+        int[] input = {7, 13};
+        int expectedResult = 13;
 
-    // returnNumber will return number
+        // act
+        int actualResult = cut.findHighestNumber(input);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
