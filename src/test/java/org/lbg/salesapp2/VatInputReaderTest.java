b7786bddb78288b4b68d55fbf6bcbfd9965f053a
campanula <65985506+campanula@users.noreply.github.com>
2024-10-31 14:31:35 +0000

    salesapp2 test8

--- src/test/java/org/lbg/salesapp2/VatInputReaderTest.java
+++ src/test/java/org/lbg/salesapp2/VatInputReaderTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VatInputReaderTest {
     @Test
-    public void getValidatedVatRate_will_return_string_as_int(){
+    public void getValidatedVatRate_will_return_string_entered_as_int(){
         // arrange
         ICustomPrompt icp = new CustomPrompt();
         VatInputReader cut = new VatInputReader(icp);
@@ -21,6 +21,38 @@ public class VatInputReaderTest {
 
         // act
         int actualResult = cut.getValidatedVatRate(inputStream);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkIfInt_will_return_true_for_valid_integer(){
+        // arrange
+        ICustomPrompt icp = new CustomPrompt();
+        VatInputReader cut = new VatInputReader(icp);
+        boolean expectedResult = true;
+
+        String input = "5";
+
+        // act
+        boolean actualResult = cut.checkIfInt(input);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkIfInt_will_return_false_for_not_valid_integer(){
+        // arrange
+        ICustomPrompt icp = new CustomPrompt();
+        VatInputReader cut = new VatInputReader(icp);
+        boolean expectedResult = false;
+
+        String input = "afaf";
+
+        // act
+        boolean actualResult = cut.checkIfInt(input);
 
         // assert
         assertEquals(expectedResult, actualResult);
