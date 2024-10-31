301610c6bb5540683e401cf05b15e0f29b9fc7a9
campanula <65985506+campanula@users.noreply.github.com>
2024-10-31 14:48:37 +0000

    salesapp2 test10

--- src/test/java/org/lbg/salesapp2/VatInputReaderTest.java
+++ src/test/java/org/lbg/salesapp2/VatInputReaderTest.java
@@ -3,10 +3,14 @@ package org.lbg.salesapp2;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class VatInputReaderTest {
     @Test
@@ -72,6 +76,21 @@ public class VatInputReaderTest {
 
         // assert
         assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getInput_will_return_entered_input() {
+        // arrange
+        InputStream inContent = new ByteArrayInputStream("1".getBytes());
+        CustomPrompt icp = new CustomPrompt();
+        VatInputReader cut = new VatInputReader(icp);
+        String expectedResult = "1";
+
+        // act
+        String result = cut.getInput(inContent);
+
+        // assert
+        assertEquals(expectedResult, result);
     }
 
 }
