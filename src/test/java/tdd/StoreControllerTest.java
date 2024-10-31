e351337615599f575308871c88d10a26a8d4120c
campanula <65985506+campanula@users.noreply.github.com>
2024-10-31 14:08:30 +0000

    salesapp2 test5

--- src/test/java/tdd/StoreControllerTest.java
+++ src/test/java/tdd/StoreControllerTest.java
@@ -6,6 +6,7 @@ import org.lbg.salesapp2.StoreController;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StoreControllerTest {
 
@@ -23,6 +24,22 @@ public class StoreControllerTest {
 
         // assert
         assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void calculateVAT_will_return_the_total_of_two_numbers_as_a_decimal(){
+        // arrange
+        StoreController cut = new StoreController();
+        double expectedResult = 0.02;
+        double notExpectedResult = 2;
+
+        // act
+        double actualResult = cut.calculateVAT(1, 2);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+        assertNotEquals(notExpectedResult, actualResult);
 
     }
 }
